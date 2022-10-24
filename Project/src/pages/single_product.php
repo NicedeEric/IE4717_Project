c6a6015b75cd17d2928a6ae5fa18d5e86fa11631
ericding <ericding0110@gmail.com>
2022-10-24 16:39:42 +0800

    feat: add single product html code

--- Project/src/pages/single_product.php
+++ Project/src/pages/single_product.php
@@ -5,15 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../CSS/color.css">
+    <link rel="stylesheet" href="../CSS/base.css">
 </head>
 <style>
     .singleProduct {
         display: inline-block;
-        width: 25%;
+        width: 20%;
+        margin: 10px;
+        background-color: #fff;
+        border: 1px solid #00b0ff;
+    }
+
+    .singleProduct:hover {
+        padding-top: 2px;
+        box-shadow: 5px 5px #00b0ff;
     }
     .productImg img {
         width: 100%;
-        height: 200px;
+        height: 10%;
+    }
+    .productName {
+        font-size: 20px;
+    }
+    .price {
+        margin-left: 2%;
+        font-size: 24px;
+        color: #00b0ff;
+    }
+    .category {
+        margin-left: 70%;
+        font-size: 20px;
+        color: #ccc;
     }
 </style>
 <body>
@@ -26,7 +49,7 @@
         </div>
         <div class="productInfo">
             <span class="price">
-                product price
+                $product price
             </span>
             <span class="category">
                 category
